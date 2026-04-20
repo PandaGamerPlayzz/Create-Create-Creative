9a7407a7f5fb208a97c7a9860aa8c0f4b81769ed
PandaGamerPlayzz <zacharymiller515@gmail.com>
2026-04-20 14:11:43 -0400

    (Almost) fully working void pump
    Just needs to have the partial model replaced

--- src/main/java/com/pandagamerplayzz/createcreative/block/entity/renderer/VoidPumpRenderer.java
+++ src/main/java/com/pandagamerplayzz/createcreative/block/entity/renderer/VoidPumpRenderer.java
@@ -1,21 +1,29 @@
 package com.pandagamerplayzz.createcreative.block.entity.renderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.pandagamerplayzz.createcreative.block.entity.VoidPumpBlockEntity;
 import com.simibubi.create.AllPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
+import com.simibubi.create.foundation.blockEntity.renderer.SafeBlockEntityRenderer;
 
 import net.createmod.catnip.render.CachedBuffers;
 import net.createmod.catnip.render.SuperByteBuffer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class VoidPumpRenderer extends KineticBlockEntityRenderer<VoidPumpBlockEntity> {
-    public VoidPumpRenderer(BlockEntityRendererProvider.Context context) {
-        super(context);
-    }
+public class VoidPumpRenderer extends SafeBlockEntityRenderer<VoidPumpBlockEntity> {
+    public VoidPumpRenderer(BlockEntityRendererProvider.Context context) {}
 
     @Override
-    protected SuperByteBuffer getRotatedModel(VoidPumpBlockEntity be, BlockState state) {
-        return CachedBuffers.partialFacing(AllPartialModels.MECHANICAL_PUMP_COG, state);
+    protected void renderSafe(VoidPumpBlockEntity be, float partialTicks, PoseStack ms,
+                              MultiBufferSource buffer, int light, int overlay) {
+        BlockState state = be.getBlockState();
+
+        SuperByteBuffer cog = CachedBuffers.partialFacing(AllPartialModels.MECHANICAL_PUMP_COG, state);
+
+        KineticBlockEntityRenderer.standardKineticRotationTransform(cog, be, light)
+                .renderInto(ms, buffer.getBuffer(RenderType.cutoutMipped()));
     }
 }
